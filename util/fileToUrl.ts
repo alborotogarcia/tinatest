a2f48e85d77acff624531cfc7de13f6fb0d417a2
alborotogarcia <alborotogarcia777@gmail.com>
2022-03-15 03:33:24 +0100

    clean

--- util/fileToUrl.ts
+++
@@ -1,3 +0,0 @@
-export function fileToUrl (filepath: string, base: string = null) {
-  return filepath.replace('.json', '').replace(base, '').replace('/', '')
-}
--- util/getLocalFiles.ts
+++
@@ -1,6 +0,0 @@
-
-export const getLocalFiles = async (filePath) => {
-  // grab all json files
-  const fg = require('fast-glob')
-  return await fg(`${filePath}**/*.json`)
-}
